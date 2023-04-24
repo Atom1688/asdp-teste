f93f75a0fbf174dafd1ef795707e4495bb71679a
Atom1688 <atomthepro@outlook.com>
2023-04-24 20:08:59 -0300

    "commit inicial"

---
+++ servidor/CEtcdI.cpp
@@ -0,0 +1,63 @@
+#include "CEtcdI.h"
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+extern CORBA::ORB_var orb;
+
+CEtcd_impl::CEtcd_impl (const string& account_id)
+{
+    id_ = account_id;
+    table_ = map<string, string>();
+}
+
+string CEtcd_impl::id ()
+{
+    cout <<  "* Retornando ID" << endl;
+    return id_;
+}
+
+
+void CEtcd_impl::put(const string& key, const string& val) 
+{
+    cout <<  "* Inserindo <chave,valor>: " << endl;
+    table_[key] = val;
+    cout <<  "* " << endl;
+}
+
+
+string CEtcd_impl::get(const string& key) 
+{
+    cout <<  "* Retornando valor da chave: " << endl;
+    if (table_.find(key) == table_.end()) 
+    {
+        throw invalid_argument("InvalidKey");
+    }
+    cout <<  "* " << endl;
+    return table_[key];
+}
+
+
+void CEtcd_impl::del(const string& key) 
+{
+    cout <<  "* Deletando chave" << endl;
+    if (table_.find(key) == table_.end()) 
+    {
+        throw invalid_argument("InvalidKey");
+    }
+    cout <<  "* " << endl;
+    table_.erase(key);
+}
+
+
+void CEtcd_impl::shutdown (const string password)
+{
+	if (password == "ASDPC") {
+	    cout << "* shutdown()" << endl;
+    	orb->shutdown();
+    } else
+    	cout << "* shutdown(): senha invalida" << endl;
+}
+
+
---
+++ servidor/CEtcdI.h
@@ -0,0 +1,32 @@
+#ifndef CONTAIMPL_H_
+#define CONTAIMPL_H_
+
+#include "CEtcdS.h"
+#include <string>
+#include <map>
+
+using namespace std;
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif
+
+class CEtcd_impl : public virtual POA_CEtcd {
+public:
+    
+    CEtcd_impl(const string& account_id);
+    virtual ~CEtcd_impl () {}
+    
+    virtual string id();
+    virtual void put(const string& key, const string& val);
+    virtual string get(const string& key);
+    virtual void del(const string& key);
+    virtual void shutdown (const string password);
+
+private:
+
+    string id_;
+    map<string, string> table_;
+};
+
+#endif /* CONTAIMPL_H_  */
